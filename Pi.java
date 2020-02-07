e52e8d072dff53975ff414ff40953c3aeb2fa248
Kebung <60605027+Kebung@users.noreply.github.com>
2020-02-07 01:02:22 +0100

    Add files via upload

---
+++ Pi.java
@@ -0,0 +1,8 @@
+public class Pi {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+        double sin = Math.sin(Math.PI / 2);
+        System.out.println(sin);
+    }
+}
---
+++ Silnia.java
@@ -0,0 +1,14 @@
+class Silnia {
+    public int silnia(int i)
+    {
+        if (i == 0)
+            return 1;
+        else
+            return i * silnia(i - 1);
+    }
+    static public void main(String argv[])
+    {
+        Silnia s = new Silnia();
+        System.out.println(s.silnia(4));
+    }
+}
